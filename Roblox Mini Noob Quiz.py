530b9fa6ff8a3a5c8410080e3574ed89df5990c0
csMondor <148488384+csMondor@users.noreply.github.com>
2023-10-19 11:27:10 -0700

    Add files via upload
    My first project. A simple text quiz I made for my kids, based around the popular kids game, Roblox.

---
+++ Roblox Mini Noob Quiz.py
@@ -0,0 +1,62 @@
+print("Welcome to the ROBLOX QUIZ for Noobs")
+print("If you can't pass this quiz, you're a ROBLOX NOOB!!!")
+
+playing = input("Are you sure you want to play? yes or no ")
+if playing.lower() == "yes":
+    print("Let's get going!")
+else:
+    print("see ya NOOB!")
+    quit()
+print("PRESS ENTER")
+input()
+answer1 = input("Ok Noob, First Question \n What does the name Roblox actually mean? \n 1= Its short for Roadblocks \n 2= It's a combination of robot and blocks\n 3= It's the last name of the creator\n ")
+if answer1 == "2":
+    print("Wow first one right!, but this is just the beginning!")
+else:
+    print("Bruh, you really are a noob, i kinda feel bad")
+    print("try again... i guess")
+    quit()
+print("PRESS ENTER")
+input()
+answer2 = input("Second question! How many hats can a character wear at once?\n1=One hat\n2=Three Hats\n3=As many hats as you want\n ")
+if answer2 == "2":
+    print("Alright, alright so you've played a little bit... maybe.\nBut the next one wont be so easy")
+else:
+    print("I knew it was beginners luck!! Try again Noob!")
+    quit()
+print("PRESS ENTER")
+input()
+answer3 = input("Third Question!\nHow many people play Roblox every month on average?\n1= 30,000\n2= 3,000,000\n3= 30,000,000\n")
+if answer3 == "3":
+    print("Dang it! I thought I had you with that one!\nOk, carry on.")
+else:
+    print("I knew I would get you with that one, NOOB!")
+    print("Try again!!")
+    quit()
+print("PRESS ENTER")
+input()
+print("Ok ok, question number 4!\nWhat is the original name of roblox?")
+print("1= Dyna Blocks")
+print("2= Extra Blocks")
+print("3= Block Builders")
+answer4 = input()
+if answer4 == "1":
+    print("What!?! Does your uncle work for Roblox or something?\nHow did you know that?")
+else:
+    print("I bet you though you had this one, huh noob?\nMaybe next time")
+    quit()
+print("PRESS ENTER")
+input()
+print("Ok, final question!!!")
+print("What is the name of the platform you can use to build your own worlds and games?")
+print("1= Developer Arena")
+print("2= Roblox Studio")
+print("3= World Builder")
+answer5 = input()
+if answer5 == "2":
+    print("CONGRATULATIONS!!!!\nYou can finally tell your noob friends that you're not a noob. Good job!")
+else:
+    print("You made it all this way, but guess what!?!?\nYOU'RE STILL A NOOB!!!\nGo back to your noob friends or try again!")
+    quit()
+print("PRESS ENTER TO EXIT")
+input()
